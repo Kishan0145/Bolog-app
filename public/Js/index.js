12534df8073dd72186978a6b0d0b0ef131c69417
Kishan Sharma <kishansharma0145@gmail.com>
2021-07-03 12:38:01 +0530

    removing errors

--- public/Js/index.js
+++ public/Js/index.js
@@ -39,19 +39,19 @@ document.querySelector(".searchButton").addEventListener("click", () => {
         location.href = `?search=${search}`;
     }
 })
-// addEventListener("load", () => {
-//     var start = Date.now(); // The current date (in miliseconds)
-//     var end = start + 1000; // 5 seconds afterwords
+addEventListener("load", () => {
+    var start = Date.now(); // The current date (in miliseconds)
+    var end = start + 1000; // 5 seconds afterwords
 
-//     function spinWheel() {
-//         start = Date.now(); // Get the date currently
-//         /* Your code here */
-//         if (start > end){
-//             document.querySelector(".loader").style.display = "none";
-//             document.querySelector(".load").style.display = "block";
-//             clearInterval(timer); // If we are 5 seconds later clear interval
-//         }
-//     }
-//     var timer = setInterval(spinWheel, 100);
+    function spinWheel() {
+        start = Date.now(); // Get the date currently
+        /* Your code here */
+        if (start > end){
+            document.querySelector(".loader").style.display = "none";
+            document.querySelector(".load").style.display = "block";
+            clearInterval(timer); // If we are 5 seconds later clear interval
+        }
+    }
+    var timer = setInterval(spinWheel, 100);
       
-// })
+})
